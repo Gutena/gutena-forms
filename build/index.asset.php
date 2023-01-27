bf1a6ba0fa65c1d67db2b801a75ebbdc7b2aec13
randhirexpresstech <randhir@expresstech.io>
2023-01-27 10:25:09 +0530

    Fixed missing form identity issue

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n'), 'version' => 'f94fee3d51841102d4b8');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n'), 'version' => '7faf35fbbbe53494d9a7');
