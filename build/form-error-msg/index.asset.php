756e3c74ab619f086033693f458069686d5a3930
randhirexpresstech <randhir@expresstech.io>
2023-02-07 12:04:27 +0530

    revert ancestor block to parent block in block.json in child blocks

--- build/form-error-msg/index.asset.php
+++ build/form-error-msg/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '69204380b5e2272022d4');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'dd83cbe64c1fc69d6872');
--- build/form-field/index.asset.php
+++ build/form-field/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '4e09ebb56e2c2b9a1a1c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '318cad7f43180f649590');
