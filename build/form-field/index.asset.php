bfca23eacc0010fc48fce2e23821b968a168b469
randhirexpresstech <randhir@expresstech.io>
2023-02-07 10:57:43 +0530

    remove console log

--- build/form-field/index.asset.php
+++ build/form-field/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '8363dea2e7d557dff423');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '4e09ebb56e2c2b9a1a1c');
