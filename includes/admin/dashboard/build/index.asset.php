9ad07ad43d59821f68209602134d97912b600a2c
randhirexpresstech <randhir@expresstech.io>
2023-07-13 13:25:21 +0530

    update introduction page

--- includes/admin/dashboard/build/index.asset.php
+++ includes/admin/dashboard/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'fe13abb676be4bbf849b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'c88d9ea9574e0d3da887');
