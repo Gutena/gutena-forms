1909f3465a8b82b90e1a5185fdb6a3fa41255903
syedaliObjects <syedali@objects.email>
2025-10-08 12:18:45 +0500

    GF-11 - Update Freemius integration: set 'has_addons' to false and add plugin dependency notice

--- includes/freemius.php
+++ includes/freemius.php
@@ -28,7 +28,7 @@ if ( ! function_exists( 'gutena_forms__fs' ) ) :
 					'type'           => 'plugin',
 					'public_key'     => 'pk_d66286e6558c1d5d6a4ccf3304cfb',
 					'is_premium'     => false,
-					'has_addons'     => true,
+					'has_addons'     => false,
 					'has_paid_plans' => false,
 					'menu'           => array(
 						'slug'    => 'gutena-forms',
