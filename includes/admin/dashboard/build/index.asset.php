6a7bd250a11547844a8e61248f16a98297e5efbc
syedaliObjects <syedali@objects.email>
2025-11-07 12:40:53 +0500

    GF-30 - Update version and add Google Fonts to introduction page styles

--- includes/admin/dashboard/build/index.asset.php
+++ includes/admin/dashboard/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '5118a2549f49ef57a90b');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '989ce2e7b2233e8d2877');
