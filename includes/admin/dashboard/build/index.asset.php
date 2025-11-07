737b9b94f4b4470fddab7b2494d4059a07185ec2
syedaliObjects <syedali@objects.email>
2025-11-07 12:16:07 +0500

    GF-19 - Enhance onboarding screen with updated icons, improved layout, and added video tutorial functionality

--- includes/admin/dashboard/build/index.asset.php
+++ includes/admin/dashboard/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'd981c37d2888813cbe01');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '989ce2e7b2233e8d2877');
