9b1bb295a3102729a3c3c2ba7d0f474f2bac782d
syedaliObjects <syedali@objects.email>
2025-12-15 17:41:57 +0500

    Update version in index.asset.php to reflect latest changes

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-i18n'), 'version' => 'bf3d4d8b0c964f4b8e0b');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-i18n'), 'version' => '2739b7d0222a2816cb2e');
