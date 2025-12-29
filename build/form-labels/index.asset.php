1430f69744a9f793d7e6af7b5ea346eacc39e532
syedaliObjects <syedali@objects.email>
2025-12-29 13:20:46 +0500

    Update dependencies and enhance form label functionality for improved accessibility

--- build/form-labels/index.asset.php
+++ build/form-labels/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '2c1bffc2513165f2c01c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element'), 'version' => 'f22fec96dbafd15596aa');
