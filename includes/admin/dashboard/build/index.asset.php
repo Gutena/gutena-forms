447b005cb4c83b025a6a96df31c4c789be82e52d
syedaliObjects <syedali@objects.email>
2026-01-05 17:24:49 +0500

    Update asset versions in index.asset.php and index.js

--- includes/admin/dashboard/build/index.asset.php
+++ includes/admin/dashboard/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '172a057332a38192d2ae');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '432c3f3a9ca3dacc7a1b');
