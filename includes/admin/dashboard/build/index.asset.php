67fc50c1d71bd29fc3d65549fcec5b5ef1f046a9
syedaliObjects <syedali@objects.email>
2026-02-11 16:21:18 +0500

    Add user role management features; implement updateUserRole API and UI for role updates

--- includes/admin/dashboard/build/index.asset.php
+++ includes/admin/dashboard/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => 'c7ffe1721e3d3a1cab77');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '763957b7aa5ca1ea6283');
