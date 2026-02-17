6fa0135ad9813efed38e4450d143bfd56bf1e61a
syedaliObjects <syedali@objects.email>
2026-02-17 16:43:45 +0500

    GF-173 - Refactor integration settings layout for improved responsiveness and update asset version

--- includes/admin/dashboard/build/index.asset.php
+++ includes/admin/dashboard/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => 'bab17218619a0b064222');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '1e3d99f0448c50e38387');
