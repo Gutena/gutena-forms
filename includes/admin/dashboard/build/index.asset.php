50a6491d48aadfe1e5cd2b5dc2a1a85d5dc0b231
syedaliObjects <syedali@objects.email>
2026-03-03 11:43:18 +0500

    GF-203 - Update font size in styles and increment asset version

--- includes/admin/dashboard/build/index.asset.php
+++ includes/admin/dashboard/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '4ce5b35f6ce6d4088c11');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '0e4be4401a2d33ddbf4c');
