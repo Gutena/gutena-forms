63ea588dd2601fa895edcce5bc69db81efd4525e
syedaliObjects <syedali@objects.email>
2026-03-03 11:39:18 +0500

    GF-212 - Add disabled state styles for buttons and update asset version

--- includes/admin/dashboard/build/index.asset.php
+++ includes/admin/dashboard/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => 'c5bbc97a76d9f53c7a49');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '4ce5b35f6ce6d4088c11');
