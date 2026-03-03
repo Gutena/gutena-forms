7cdef9d6c5709a096aa1fafae284fe5fdd4c396b
syedaliObjects <syedali@objects.email>
2026-03-03 14:26:23 +0500

    GF-204 - Update color scheme in styles and increment asset version

--- includes/admin/dashboard/build/index.asset.php
+++ includes/admin/dashboard/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => 'a46443ea197a90d240f5');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => 'c2aa37a30059047007d8');
