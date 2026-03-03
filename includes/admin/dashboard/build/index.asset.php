9ccacc3b73004e6ffc41f74a0ee33a8ae857471e
syedaliObjects <syedali@objects.email>
2026-03-03 11:53:41 +0500

    GF-206 - Update asset version and enhance styles for better layout and readability

--- includes/admin/dashboard/build/index.asset.php
+++ includes/admin/dashboard/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '0e4be4401a2d33ddbf4c');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '19ca0bb0b935b5c53cc5');
