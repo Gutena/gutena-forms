46b174a4bedee425ab4c31a30aa4d891d62ae8c5
syedaliObjects <syedali@objects.email>
2026-03-26 11:39:00 +0500

    GF-141 - Update asset versions in index.asset.php for improved dependency management code merge

--- includes/admin/dashboard/build/index.asset.php
+++ includes/admin/dashboard/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => 'cd6a04029b9cb586a85d');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '01528cf655de92d7d674');
