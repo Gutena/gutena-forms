46cda2775cb57edd1396d54b420e6b6cd2f32c38
syedaliObjects <syedali@objects.email>
2026-03-26 11:41:48 +0500

    GF-141 - Update asset version and adjust layout dimensions in index.scss for improved styling

--- includes/admin/dashboard/build/index.asset.php
+++ includes/admin/dashboard/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '01528cf655de92d7d674');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '9749ac5ba5d6f828f08d');
