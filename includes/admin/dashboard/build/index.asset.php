b1ddf0e6fbd49c76ccd364446dd69c89a59f888c
syedaliObjects <syedali@objects.email>
2026-03-26 11:52:12 +0500

    GF-141 - Enhance radio group control styles and update asset version for improved UI consistency

--- includes/admin/dashboard/build/index.asset.php
+++ includes/admin/dashboard/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '9749ac5ba5d6f828f08d');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => 'f7dcc7f553d28f26e4c7');
