b4d466169d5a91e0ee78df2bcf42571ab1722332
syedaliObjects <syedali@objects.email>
2026-04-02 12:06:58 +0500

    GF-256 - Fix loading skeleton condition and update asset version

--- includes/admin/dashboard/build/index.asset.php
+++ includes/admin/dashboard/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '4e82f3bd264fda548a23');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '1080e7ac830ba49bae5f');
