739fbbc4c35ed522448202fafdff7ea48a2b0585
syedaliObjects <syedali@objects.email>
2026-04-02 12:34:09 +0500

    GF-250 - Update asset versions in index.asset.php and index.js

--- includes/admin/dashboard/build/index.asset.php
+++ includes/admin/dashboard/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => 'bf74a4f70b64070d154a');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => 'a04958055d458853169c');
