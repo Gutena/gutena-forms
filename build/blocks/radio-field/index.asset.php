02b3c216d59750f4640859b7a7a196fd3f3f7478
syedaliObjects <syedali@objects.email>
2026-04-09 17:08:22 +0500

    GF-248 - Add standalone field group blocks for date, time, url, phone, password, hidden, country, state, and rating fields

--- build/blocks/radio-field/index.asset.php
+++ build/blocks/radio-field/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '4dbb1ab6641b5e19bc6c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b9a979601f36f8642bc1');
