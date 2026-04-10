6f28223e2342231c65528fc67421a0e802c1e198
syedaliObjects <syedali@objects.email>
2026-04-10 10:14:16 +0500

    GF-141-2 - Update asset versions in script and index files for improved consistency

--- includes/admin/dashboard/build/index.asset.php
+++ includes/admin/dashboard/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => 'bd9f31fc02c5d54d9f79');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '693a522d0526ed770ad6');
