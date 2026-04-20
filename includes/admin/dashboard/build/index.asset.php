b4aab0a30fe040092ada54e82fee55bab1101d46
syedaliObjects <syedali@objects.email>
2026-04-20 13:28:27 +0500

    GF-305 - Update asset version and adjust styles for Gutena Forms layout and links

--- includes/admin/dashboard/build/index.asset.php
+++ includes/admin/dashboard/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '8f59553769dc262e7dfc');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '2c87da776bc449fe21d0');
