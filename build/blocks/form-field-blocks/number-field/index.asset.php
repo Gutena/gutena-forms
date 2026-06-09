ad365d39736abc04439aa1f19648b66c07eeab70
syedaliObjects <syedali@objects.email>
2026-06-09 15:41:11 +0500

    GF-383 - Fixed issue related to settings alignment in Number Field and Range Field

--- build/blocks/form-field-blocks/number-field/index.asset.php
+++ build/blocks/form-field-blocks/number-field/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '52b4681581436c0a32e0');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'bd1febe213cf95f7912f');
