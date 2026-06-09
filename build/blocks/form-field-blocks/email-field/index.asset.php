b3e28fb24eaa1c13f0ae644577366c4523ffc668
syedaliObjects <syedali@objects.email>
2026-06-09 16:21:42 +0500

    GF-389 - Fixed issue when default value not assigning when form loads

--- build/blocks/form-field-blocks/email-field/index.asset.php
+++ build/blocks/form-field-blocks/email-field/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '77a41f051df95b0f6187');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b2db938178d32b1c2674');
--- build/blocks/form-field-blocks/textarea-field/index.asset.php
+++ build/blocks/form-field-blocks/textarea-field/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '1c0fcd8de9405a227428');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '1a6afbe900e1cc607c8b');
