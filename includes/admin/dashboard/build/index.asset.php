28da13c97c8f4d55ba9f0960689f089d6922d1bf
syedaliObjects <syedali@objects.email>
2026-08-27 17:46:35 +0500

    GF-520 - resolved responsive issues

--- includes/admin/dashboard/build/index.asset.php
+++ includes/admin/dashboard/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '39750c631e491432406c');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '156add01a690ff172731');
