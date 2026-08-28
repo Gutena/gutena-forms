ff55e46e90cb76b0e3eda5b856bc51d6b2d9f2b3
syedaliObjects <syedali@objects.email>
2026-08-28 10:43:00 +0500

    GF-520 - UI issues resolved
    GF-520 - resolved responsive issues desktop, mobile

--- includes/admin/dashboard/build/index.asset.php
+++ includes/admin/dashboard/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '85b4f12c1e1fc043dcdc');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => 'eb6ed2875b00b72e912e');
